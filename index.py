3f02fbffcf8047d8a65b5262446997592b3ad6f5
xuan2003 <s1102294@gm.pu.edu.tw>
2022-10-18 09:40:03 +0800

    make it better

---
+++ index.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, request
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    homepage = "<h1>楊子青Python網頁</h1>"
+    homepage += "<a href=/mis>MIS</a><br>"
+    homepage += "<a href=/today>顯示日期時間</a><br>"
+    homepage += "<a href=/welcome?nick=tcyang>傳送使用者暱稱</a><br>"
+    homepage += "<a href=/account>網頁表單輸入實例</a><br><br>"
+    homepage += "<a href=/about>子青簡介網頁</a><br>"
+    return homepage
+
+@app.route("/mis")
+def course():
+    return "<h1>資訊管理導論</h1>"
+
+@app.route("/today")
+def today():
+    now = datetime.now()
+    return render_template("today.html", datetime = str(now))
+
+@app.route("/welcome", methods=["GET", "POST"])
+def welcome():
+    user = request.values.get("nick")
+    return render_template("welcome.html", name=user)
+
+@app.route("/about")
+def about():
+    return render_template("aboutme.html")
+
+@app.route("/account", methods=["GET", "POST"])
+def account():
+    if request.method == "POST":
+        user = request.form["user"]
+        pwd = request.form["pwd"]
+        result = "您輸入的帳號是：" + user + "; 密碼為：" + pwd 
+        return result
+    else:
+        return render_template("account.html")
+
+#if __name__ == "__main__":
+ #   app.run()
